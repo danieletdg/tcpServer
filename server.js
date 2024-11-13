5a914788cf5fc2c994fb22ee631a401d494faeb3
dbalzano <daniele@filoblu.com>
2024-11-13 14:56:09 +0100

    fix

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ const tcpServer = net.createServer(
     }
 )
 
-tcpServer.listen(3001, 'localhost', function() {
+tcpServer.listen(3001, '0.0.0.0', function() {
     console.log('Server listening on port 3001');
 });
